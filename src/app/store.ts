a82722f75c7f28e87a8a1c6101da04aeab4d2d7d
Asya I <as.izmaylova@gmail.com>
2025-06-24 14:36:43 +0300

    ll

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,24 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todosReducer from "../features/todos/todoSlice"
-import { Todo } from "../features/todos/types";
+// import { Todo } from "../features/todos/types";
 
-const savedTodoList: Todo[] = JSON.parse(localStorage.getItem("todoList") || "[]");
+// const savedTodoList: Todo[] = JSON.parse(localStorage.getItem("todoList") || "[]");
 
 export const store = configureStore({
     reducer: {
         todos: todosReducer,
     },
-    preloadedState: {
-        todos: {
-            todos: savedTodoList
-        }
-    }
+    // preloadedState: {
+    //     todos: {
+    //         todos: savedTodoList
+    //     }
+    // }
 })
 
-store.subscribe(() => {
-    const state = store.getState();
-    localStorage.setItem("todoList", JSON.stringify(state.todos.todos));
-})
+// store.subscribe(() => {
+//     const state = store.getState();
+//     localStorage.setItem("todoList", JSON.stringify(state.todos.todos));
+// })
 
 // returntype - встроенный дженерик (тип тайпскрипта), 
 // получает тип возвращаемого значения функции
